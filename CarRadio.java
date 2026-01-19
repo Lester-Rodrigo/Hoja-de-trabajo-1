f52db66ff4be1d921e79c99c2c2b66f07c7924c7
Lester-Rodrigo <alrodrinew@gmail.com>
2026-01-19 10:03:06 -0600

    Classe CarRadio programada, a espera de tests unitarios

--- CarRadio.java
+++ CarRadio.java
@@ -1,3 +1,89 @@
-public class CarRadio{
+public class CarRadio implements Radio {
+    //Variables
+    private boolean OnOff;
+    private String modulation;
+    private float currentStation;
+    private float[] favoriteStation;
 
+    public CarRadio() {
+        this.OnOff = false;
+        this.modulation = "AM";
+        this.currentStation = 530f;
+        this.favoriteStation = new float[12]; 
+    }
+
+    @Override
+    public void PowerOn() {
+        OnOff = true;
+    }
+
+    @Override
+    public void ChangeModulation() {
+        if (modulation.equals("AM")) {
+            modulation = "FM";
+            currentStation = 87.9f;
+        } else {
+            modulation = "AM";
+            currentStation = 530f;
+        }
+        System.out.println("Modulation changed to " + modulation);
+    }
+
+    @Override
+    public void MoveStationForward() {
+        if (modulation.equals("AM")) {
+            currentStation += 10f;
+            if (currentStation > 1610f) currentStation = 530f; 
+        } else {
+            currentStation += 0.2f;
+            if (currentStation > 107.9f) currentStation = 87.9f;
+        }
+        System.out.println("Estación: " + currentStation + " " + modulation);
+    }
+
+    @Override
+    public void MoveStationBackward() {
+       if (modulation.equals("AM")) {
+            currentStation -= 10f;
+            if (currentStation < 530f) currentStation = 1610f; 
+        } else {
+            currentStation -= 0.2f;
+            if (currentStation < 87.9f) currentStation = 107.9f;
+        }
+        System.out.println("Estación: " + currentStation + " " + modulation);
+    }
+
+    @Override
+    public void SaveToFavorite(int saveSlot) {
+        if (saveSlot < 0 || saveSlot >= favoriteStation.length) {
+            System.out.println("Error, por favor ingrese una posición válida.");
+            return;
+        }
+        favoriteStation[saveSlot] = currentStation;
+        System.out.println("Se guardó " + currentStation + " " + modulation + " en el boton número: " + saveSlot);
+    }
+
+    @Override
+    public float GoFavorite(int saveSlot) { 
+        if (saveSlot < 0 || saveSlot >= favoriteStation.length) {
+            System.out.println("Error, por favor ingrese una posición válida.");
+            return -1;
+        }
+        currentStation = favoriteStation[saveSlot];
+        if (currentStation < 530f && modulation.equals("AM")) {
+            System.out.println("La estación guardada es FM y usted se encuentra en AM, cambie la modulación.");
+            return -1;
+        }
+        else if (currentStation > 107.9f && modulation.equals("FM")) {
+            System.out.println("La estación guardada es AM y usted se encuentra en FM, cambie la modulación.");
+            return -1;
+        }
+        System.out.println("Estación actual: " + currentStation + " " + modulation);
+        return currentStation;
+    }
+
+    @Override
+    public void PowerOff() {
+        OnOff = false;
+    } 
 }
